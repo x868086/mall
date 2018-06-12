601ebba69eecdb53bd69df20a2fb54da8f9e27d4
x8086 <43070810@qq.com>
2018-06-12 23:22:49 +0800

    member0612

--- src/pages/member/components/address_form.vue
+++ src/pages/member/components/address_form.vue
@@ -5,120 +5,167 @@
         <input class="js-id" name="id" type="hidden" value="69150287">
         <div class="block-item" style="border-top:0;">
           <label>收货人</label>
-          <input type="text" placeholder="请输入姓名" v-model="name" maxlength="20">
+          <input type="text" placeholder="请输入姓名" v-model.trim="name" maxlength="20">
         </div>
         <div class="block-item">
           <label>联系电话</label>
-          <input type="tel" placeholder="联系电话" v-model="tel" maxlength="11">
+          <input type="tel" placeholder="联系电话" v-model.trim="tel" maxlength="11">
         </div>
         <div class="block-item">
           <label>选择地区</label>
           <div class="select-group">
-            <select class="js-province-selector">
+            <select class="js-province-selector" v-model="provinceValue">
               <option value="-1">选择省份</option>
-              <option value="110000">北京市</option>
-              <option value="120000">天津市</option>
-              <option value="130000">河北省</option>
-              <option value="140000">山西省</option>
-              <option value="150000">内蒙古自治区</option>
-              <option value="210000">辽宁省</option>
-              <option value="220000">吉林省</option>
-              <option value="230000">黑龙江省</option>
-              <option value="310000">上海市</option>
-              <option value="320000">江苏省</option>
-              <option value="330000">浙江省</option>
-              <option value="340000">安徽省</option>
-              <option value="350000">福建省</option>
-              <option value="360000">江西省</option>
-              <option value="370000">山东省</option>
-              <option value="410000">河南省</option>
-              <option value="420000">湖北省</option>
-              <option value="430000">湖南省</option>
-              <option value="440000">广东省</option>
-              <option value="450000">广西壮族自治区</option>
-              <option value="460000">海南省</option>
-              <option value="500000">重庆市</option>
-              <option value="510000">四川省</option>
-              <option value="520000">贵州省</option>
-              <option value="530000">云南省</option>
-              <option value="540000">西藏自治区</option>
-              <option value="610000">陕西省</option>
-              <option value="620000">甘肃省</option>
-              <option value="630000">青海省</option>
-              <option value="640000">宁夏回族自治区</option>
-              <option value="650000">新疆维吾尔自治区</option>
-              <option value="710000">台湾省</option>
-              <option value="810000">香港特别行政区</option>
-              <option value="820000">澳门特别行政区</option>
+              <option v-bind:value="pro.value" 
+              v-for="pro in addressData" 
+              v-bind:key="pro.value"
+              >{{pro.label}}</option>
             </select>
-            <select class="js-city-selector">
+            <select class="js-city-selector" v-model="cityValue">
               <option value="-1">选择城市</option>
-              <option value="440100">广州市</option>
-              <option value="440200">韶关市</option>
-              <option value="440300">深圳市</option>
-              <option value="440400">珠海市</option>
-              <option value="440500">汕头市</option>
-              <option value="440600">佛山市</option>
-              <option value="440700">江门市</option>
-              <option value="440800">湛江市</option>
-              <option value="440900">茂名市</option>
-              <option value="441200">肇庆市</option>
-              <option value="441300">惠州市</option>
-              <option value="441400">梅州市</option>
-              <option value="441500">汕尾市</option>
-              <option value="441600">河源市</option>
-              <option value="441700">阳江市</option>
-              <option value="441800">清远市</option>
-              <option value="441900">东莞市</option>
-              <option value="442000">中山市</option>
-              <option value="445100">潮州市</option>
-              <option value="445200">揭阳市</option>
-              <option value="445300">云浮市</option>
+              <option v-bind:value="c.value"
+              v-for="c in cityList"
+              v-bind:key="c.value">{{c.label}}</option>
             </select>
-            <select class="js-county-selector" name="area_code" data-code="440402">
+            <select class="js-county-selector" name="area_code" data-code="440402"
+            v-model="districtValue">
               <option value="-1">选择地区</option>
-              <option value="440402">香洲区</option>
-              <option value="440403">斗门区</option>
-              <option value="440404">金湾区</option>
+              <option
+              v-for="d in districtList" 
+              v-bind:key="d.value"
+              v-bind:value="d.value"
+              >{{d.label}}</option>
             </select>
           </div>
         </div>
         <div class="block-item">
           <label>详细地址</label>
           <input type="text" placeholder="街道门牌信息" name="address_detail"  
-          v-model="address" maxlength="100">
+          v-model.trim="address" maxlength="100">
         </div>
       </div>
     </div>
-    <div class="block section js-save block-control-btn">
+    <div class="block section js-save block-control-btn"
+    v-on:click="add">
       <div class="block-item c-blue center">保存</div>
     </div>
-    <div class="block section js-delete block-control-btn" v-show="type==='edit'">
+    <div class="block section js-delete block-control-btn" v-show="type==='edit'"
+    v-on:click="remove">
       <div class="block-item c-red center">删除</div>
     </div>
-    <div class="block stick-bottom-row center js-save-default" v-show="type==='edit'">
+    <div class="block stick-bottom-row center js-save-default" v-show="type==='edit'"
+    v-on:click="setDefault">
       <button class="btn btn-standard js-save-default-btn">设为默认收货地址</button>
     </div>
   </div>
 </template>
 
 <script>
+import addressData from 'js/address.json';
+import addressService from 'js/addressService.js';
+
 export default {
   data:function(){
     return {
       name: "",
       tel:"",
-      provinceName: "",
-      cityName: "",
-      districtName: "",
+      provinceValue: -1,
       address: "",
       cityValue: -1,
       districtValue: -1,
-      type:this.$route.query.type   //从路由对象获取当前页面的查询字符串，以此区别是edit还是add
+      type:null,
+      addressData:addressData.list,
+      cityList:null,
+      districtList:null,
+      instance:null,
+      id:""
+    }
+  },
+  
+  created:function(){
+    //组件在路由视图加载时执行，取all页面编辑模式传出的编辑对象list
+    this.type=this.$route.query.type;   //当加载当前页面后从路由对象获取当前页面的查询字符串，以此区别是edit还是add
+    this.instance=this.$route.query.list; //加载当前页面后从路由对象获取当前页面的查询字符串中的list对象
+    if(this.type==='edit'){
+      let data=this.instance
+      this.name=data.name;
+      this.tel=data.tel;
+      this.address=data.address;
+      this.id=data.id;
+      this.provinceValue=parseInt(data.provinceValue);
+      this.cityValue=parseInt(data.cityValue);
+      this.districtValue=parseInt(data.districtValue);
+    }
+  },
+  
+  watch:{
+    //监测省编码变化，如变化取到市列表
+    provinceValue:function(val){
+      if(val === -1){
+        return
+      }
+      let list=this.addressData;
+      let index=list.findIndex(item=>{
+          return item.value === val
+      })
+      this.cityList = list[index].children;
+      //取到市列表后将，市、区下拉列表还原成初始值
+      this.cityValue = -1;
+      this.districtValue = -1;
+
+      if(this.type==='edit'){
+        this.cityValue=parseInt(this.instance.cityValue)
+      }
+    },
+
+    //监测市编码变化，如变化取到区列表
+    cityValue:function(val){
+      if(val === -1){
+        return
+      }
+      let list=this.cityList;
+      let index=list.findIndex(item=>{
+          return item.value === val
+      })
+      this.districtList = list[index].children;
+      //获取到区列表后，将区下拉列表还原成初始值
+      this.districtValue = -1;
+
+      if(this.type==='edit'){
+        this.districtValue=parseInt(this.instance.districtValue)
+      }
     }
   },
   methods:{
+    add:function(){
+          //Vue 实例也代理 data 对象上所有的属性，因此访问 this.a 等价于访问 this.$data.a
+          let {name,tel,provinceValue,cityValue,districtValue,address} =this.$data;
+          let data={name,tel,provinceValue,cityValue,districtValue,address};
+      if(this.type==='add'){
+          addressService.add(data).then(res=>{
+            this.$router.push({name:'alladdress'})
+          })
+      }
 
+      if(this.type==='edit'){
+        data.id=this.id
+        addressService.update(data).then(res=>{
+          this.$router.push({name:'alladdress'})
+        })
+      }
+    },
+
+    remove:function(){
+      addressService.remove({id:this.id}).then(res=>{
+        this.$router.push({name:'alladdress'})
+      })
+    },
+
+    setDefault:function(){
+      addressService.default({id:this.id}).then(res=>{
+        this.$router.push({name:'alladdress'})
+      })
+    }
   }
 }
 </script>
